27b4a68761b056015fc1279771a7cac372445bc5
ThePiklOfTime <nicotolvanen@gmail.com>
2026-04-04 18:24:11 +0300

    Added 2 methods to GameManager.java. generateMonster and getLatestMonster

--- app/src/main/java/com/example/viikko12/GameManager.java
+++ app/src/main/java/com/example/viikko12/GameManager.java
@@ -1,7 +1,10 @@
 package com.example.viikko12;
 
+import java.util.Random;
+
 public class GameManager {
     private Player player;
+    private Monster latestMonster;
     private static GameManager gameManager;
     public static GameManager getInstance() {
         if (gameManager == null) {
@@ -12,5 +15,23 @@ public class GameManager {
 
     public Player getPlayer() {
         return this.player;
+    }
+    public Monster generateMonster(){
+        Random random = new Random();
+        int helloWorld = random.nextInt(10);
+        if (helloWorld > 5) {
+            Skeleton skeleton = new Skeleton();
+            latestMonster = skeleton;
+            return skeleton;
+        }else {
+            Vampire vampire = new Vampire();
+            latestMonster = vampire;
+            return vampire;
+        }
+
+    }
+
+    public Monster getLatestMonster() {
+        return latestMonster;
     }
 }
